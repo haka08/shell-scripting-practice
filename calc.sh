fc6d62225459bd9da8d1548d4cfe7746bbd8a0b8
haka08 <91915361+haka08@users.noreply.github.com>
2021-11-10 03:44:00 +0800

    Welcoming User

--- calc.sh
+++ calc.sh
@@ -1,10 +1,15 @@
 #! /usr/bin/env bash
 
 # When the program is first loaded, display a greeting to the user.
+echo "Hello! Welcome to Calculations made Easy!"
+
+while true; do
+
 # Then, display a menu that outlines the possible operations:
   # Add
   # Subtract
   # Exit
+
 # Then, capture the user selection.
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
